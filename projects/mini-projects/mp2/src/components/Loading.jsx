46ebdad0538e5eb19cc6a6c66c5f13c84924899a
Youngjin <yjshin229@gmail.com>
2024-02-18 06:50:05 -0500

    modify loading cat image

--- projects/mini-projects/mp2/src/components/Loading.jsx
+++ projects/mini-projects/mp2/src/components/Loading.jsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
-import jumpingCat from "../assets/jumpingCat.png";
 
 const Loading = () => {
   return (
     <StyledLoadingContainer>
-      <StyledImg src={jumpingCat} />
+      <StyledImg src={"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4cc95e3e-4d8a-4bf8-99c8-6166084d56ec/d99ey1q-e8cc1684-ceea-492a-b876-50ffc71201b2.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzRjYzk1ZTNlLTRkOGEtNGJmOC05OWM4LTYxNjYwODRkNTZlY1wvZDk5ZXkxcS1lOGNjMTY4NC1jZWVhLTQ5MmEtYjg3Ni01MGZmYzcxMjAxYjIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.Gks3x3Ptj8uJ2DvKsrt2OtuYLz66u4tlU81X29NXzJ4"} />
       <p>Loading ...</p>
     </StyledLoadingContainer>
   );
