6e4512de6814c1122cf71af089e5d6cf4cd98198
Youngjin <yjshin229@gmail.com>
2024-03-01 13:48:53 -0500

    add menu bar

--- projects/mini-projects/mp3/src/components/Header.jsx
+++ projects/mini-projects/mp3/src/components/Header.jsx
@@ -1,13 +1,36 @@
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
+import { TiThMenuOutline } from "react-icons/ti";
+import { useState } from "react";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <StyledDiv>
-      <Link to={"/"} style={{ textDecoration: "none" }}>
-        <StyledText>Youngjin Shin's Portfolio 👾</StyledText>
-      </Link>
-    </StyledDiv>
+    <>
+      <StyledDiv>
+        <Link to={"/"} style={{ textDecoration: "none" }}>
+          <StyledText>Youngjin Shin's Portfolio 👾</StyledText>
+        </Link>
+        <TiThMenuOutline
+          size={24}
+          onClick={() => [setIsMenuOpen(!isMenuOpen), console.log(isMenuOpen)]}
+        />
+      </StyledDiv>
+      <Menu isOpen={isMenuOpen} />
+    </>
+  );
+};
+
+const Menu = ({ isOpen }) => {
+  return (
+    <StyledMenuContainer style={{ display: isOpen ? "flex" : "none" }}>
+      <StyledMenu>
+        <StyledLink to={"/"}>Home</StyledLink>
+      </StyledMenu>
+      <StyledMenu>
+        <StyledLink to={"/projects"}>Projects</StyledLink>
+      </StyledMenu>
+    </StyledMenuContainer>
   );
 };
 
@@ -15,12 +38,45 @@ export default Header;
 
 const StyledDiv = styled.div`
   display: flex;
+  flex-direction: row;
   background-color: #b58dca;
   padding: 0.2rem 1rem;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const StyledText = styled.h1`
   font-family: "Kode Mono", monospace;
   color: #474747;
+`;
+
+const StyledMenuContainer = styled.ul`
+  font-family: "Kode Mono", monospace;
+  flex-direction: column;
+  background-color: beige;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  right: 0;
+  list-style-type: none;
+  width: 200px;
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledMenu = styled.li`
+  display: flex;
+  width: 200px;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    background-color: #faf0af;
+    cursor: pointer;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  padding: 1rem 0rem;
 `;
