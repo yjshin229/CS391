448884726a06391ca105f2188a5ebd95d4e77c8d
Youngjin <yjshin229@gmail.com>
2024-03-02 22:41:17 -0500

    add basename to route

--- projects/mini-projects/mp3/src/main.jsx
+++ projects/mini-projects/mp3/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "*",
-    Component: App,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "*",
+      Component: App,
+    },
+  ],
+  { basename: "/yjshin29/cs391/projects/mini-projects/mp3/dist" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
