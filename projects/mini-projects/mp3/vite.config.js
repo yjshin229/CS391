964d464f12f2aaeadc48de9ecdec8e9bd17b65f5
Youngjin <yjshin229@gmail.com>
2024-03-04 00:10:17 -0500

    remove base in config

--- projects/mini-projects/mp3/vite.config.js
+++ projects/mini-projects/mp3/vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production"
-      ? "/cs391/projects/mini-projects/mp3/dist/"
-      : "/",
+  // base:
+  //   process.env.NODE_ENV === "production"
+  //     ? "/cs391/projects/mini-projects/mp3/dist/"
+  //     : "/",
 });
