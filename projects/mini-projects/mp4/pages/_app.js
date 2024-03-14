78f94a70df354ef128a8617a27f1b801a5ea3ad7
Youngjin <yjshin229@gmail.com>
2024-03-14 00:10:47 -0700

    delete css

--- projects/mini-projects/mp4/pages/_app.js
+++ projects/mini-projects/mp4/pages/_app.js
@@ -1,5 +1,27 @@
-import "@/styles/globals.css";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @font-face {
+    font-family: 'ProximaNova-Regular';
+    src: url('/fonts/ProximaNova-Regular.woff') format('woff');
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'ProximaNova-Bold';
+    src: url('/fonts/ProximaNova-Bold.woff') format('woff');
+    font-style: bold;
+    font-weight: 700;
+    font-display: swap;
+  }
+`;
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />;
+    </>
+  );
 }
