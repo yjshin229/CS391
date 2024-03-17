8148fcb6c72f8a41eae42cd2546d68b344c6d756
Youngjin <yjshin229@gmail.com>
2024-03-17 07:53:25 -0400

    modify date format

--- projects/mini-projects/mp4/utils/DateTimeConverter.jsx
+++ projects/mini-projects/mp4/utils/DateTimeConverter.jsx
@@ -1,4 +1,13 @@
 export const UnixToDate = (unixTimestamp) => {
   const date = new Date(unixTimestamp * 1000);
-  return date.toLocaleString();
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false, // Optional: Use this line if you prefer 24-hour format
+  };
+  return date.toLocaleString("en-US", options);
 };
